75e117fcc3943b2d37aae791477201b4a28007b2
wolfgang0001 <chukwunonso53@gmail.com>
2023-09-07 11:51:21 -0700

    7

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - This is the third line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long long int c;
+	long int d;
+	float f;
+
+printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a int: %zu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
