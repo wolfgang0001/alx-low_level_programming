28a1d98a12bb716660c6805ace2cd3c1d43b6bcd
wolfgang0001 <chukwunonso53@gmail.com>
2023-09-07 11:54:17 -0700

    7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a int: %zu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a float: %zu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
