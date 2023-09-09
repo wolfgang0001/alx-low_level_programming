d78a787c5d8a86b428f5ddf8c27f6491acdcb3c5
wolfgang0001 <chukwunonso53@gmail.com>
2023-09-09 07:36:33 -0700

    six

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,17 @@
-#inculde <stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
- *
  * function to print single digit numbers
- *
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
-		int numbers;
+	int numbers;
+
 	for (numbers = 48; numbers < 50; nubers++)
-{
-		putchar(numbers);
+	{
+		printf(numbers);
+	}
+	putchar('\n');
+	return ();
 }
-		putchar(10)
-	return (0);
-}	
