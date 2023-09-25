7431369f804ea389b0c964f127e30e74f5fc105d
wolfgang0001 <chukwunonso53@gmail.com>
2023-09-25 07:30:57 -0700

    0k

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 #ifndef main_H
 #define main_H
+#include <stdio.h>
 
-int _putchar(char c)
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
