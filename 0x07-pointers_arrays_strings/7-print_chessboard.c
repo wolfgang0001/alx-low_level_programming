66c798e60c8f7c5debd25d1d5e3f4e2b6a24b50a
wolfgang0001 <chukwunonso53@gmail.com>
2023-09-26 08:45:29 -0700

    seven

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_chessboard - prints a 8 * 8  chessboard
+ * @a:the board
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+
+	while (i < 8)
+	{
+		while (j < 8)
+		{
+		_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		j = 0;
+		i++;
+	}
+}
