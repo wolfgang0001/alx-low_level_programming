2b87a662fd3407aba3ee490bb2bc044ddc05a1b7
wolfgang0001 <chukwunonso53@gmail.com>
2023-09-30 07:32:26 -0700

    zero

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,13 @@
  * _puts_recursion - function
  * @s: input
  *
- * Return: Nothing
+ * Return: Always 0 (success)
  */
 void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		putchar(*s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
