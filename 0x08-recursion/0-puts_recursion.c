0d88f6cb58b0eb42507b42e3470373c99db6179a
wolfgang0001 <chukwunonso53@gmail.com>
2023-09-30 07:12:22 -0700

    zero

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string f0llowed by a new one
+ * @s: the string to print
+ *
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
