d3425c1f883214f3c7180a9c06e46eb5c6f80e62
wolfgang0001 <chukwunonso53@gmail.com>
2023-10-02 09:58:01 -0700

    2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints the number of arguments
+ * @argc: the number of arguments
+ * @argv: the arguments
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
