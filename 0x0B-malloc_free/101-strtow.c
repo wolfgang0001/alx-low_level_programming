c31330ef2613f2aafc43225677d037297c06c9b2
wolfgang0001 <chukwunonso53@gmail.com>
2023-10-04 19:47:17 -0700

    run

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,90 @@
+#include "main.h"
+
+char *util(int start, int end, char *str);
+int strArraylen(char *str);
+
+/**
+ * strtow - splits  string into word
+ * @str: a string to be splitted
+ * Return: pointer to array of string
+ */
+char **strtow(char *str)
+{
+	char **strArr;
+	int i = 0, j = 0, len = 0, start = 0, end = 0;
+
+	if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
+		return (NULL);
+	len = strArraylen(str) - 1;
+	strArr = (char **)malloc(len * sizeof(char *) + 1);
+
+	if (strArr == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i++] != '\0')
+	{
+		if (str[i - 1] != ' ')
+		{
+			start = i - 1;
+			while (str[i] != ' ' && str[i] != '\0')
+				i++;
+			end = i;
+			strArr[j] = util(start, end, str);
+			j++;
+		}
+	}
+	strArr[len + 1] = NULL;
+	return (strArr);
+}
+
+
+
+/**
+ * strArraylen - finding the number of words in a string
+ * @str: string
+ * Return: number of words
+ */
+
+int strArraylen(char *str)
+{
+	int i = 0, len = 0;
+
+	while (str[i++] != '\0')
+	{
+		if (str[i] == ' ' && str[i - 1] != ' ')
+			len += 1;
+	}
+	return (len);
+}
+
+
+/**
+ * util - splits the words in a string and return
+ * @start: index where the word starts
+ * @end: when it ends
+ * @str: string
+ * Return: splitting words
+ */
+
+char *util(int start, int end, char *str)
+{
+	int i = 0, j = 0, len = 0;
+	char *word;
+
+	len = (end - start) + 1;
+
+	word = (char *)malloc(len * sizeof(char *));
+	if (word == NULL)
+		return (NULL);
+
+	for (i = start; i < end; i++)
+	{
+		word[j] = str[i];
+		j++;
+	}
+
+	word[end] = '\0';
+	return (word);
+}
