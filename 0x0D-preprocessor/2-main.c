5e839f3617e6e2a4c61bc776100c63a977c26001
wolfgang0001 <chukwunonso53@gmail.com>
2023-10-10 08:17:33 -0700

    two

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of the file that was compiled
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
